eeb1fe7e10752e93bca98deae55a333f1bdbeb3c
Henry Wibowo <henry.k.wibowo@gmail.com>
2021-01-31 11:29:21 +0700

    Implement solution to question #1

---
+++ 1.js
@@ -0,0 +1,55 @@
+function solution(records) {
+    const userMap = buildUserMap(records);
+    return buildChatMessages(records, userMap);
+}
+
+function buildUserMap(records) {
+    const userMap = {};
+
+    records.forEach(record => {
+        const action = getAction(record);
+        const userId = getUserId(record);
+        const nickname = getNickname(record);
+        if (action === 'Enter' || action === 'Change') {
+            userMap[userId] = nickname;
+        }
+    });
+
+    return userMap;
+}
+
+function buildChatMessages(records, userMap) {
+    messages = [];
+
+    records.forEach(record => {
+        const action = getAction(record);
+        const userId = getUserId(record);
+        const nickname = userMap[userId];
+        
+        if (action === 'Enter') {
+            messages.push(nickname + " came in.");
+        } else if (action === 'Leave') {
+            messages.push(nickname + " has left.");
+        }
+    });
+
+    return messages;
+}
+
+function getAction(record) {
+    const firstSpaceIndex = record.indexOf(' ');
+    return record.substr(0, firstSpaceIndex);
+}
+
+function getUserId(record) {
+    const elements = record.split(' ');
+    return elements[1];
+}
+
+function getNickname(record) {
+    const elements = record.split(' ');
+    return elements[2];
+}
+
+const result = solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"]);
+console.log(result);
